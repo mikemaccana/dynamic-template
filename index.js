e2b07147deb81ef1fb1e6bf8ce3b50762a9e8f92
Roland <important@van-fredenhagen.de>
2020-10-26 21:09:53 +0100

    added eval Template again

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = (templateString, templateVariables) => templateString.replace(/\${(.*?)}/g, (_, g) => templateVariables[g]);
+module.exports.saveTemplate = (templateString, templateVariables) => {
+  return templateString.replace(/\${(.*?)}/g, (_, g) => templateVariables[g]);
+};
+
+module.exports.evalTemplate = (templateString, templateVariables) => {
+  const keys = Object.keys(templateVariables);
+  const values = Object.values(templateVariables);
+  let templateFunction = new Function(...keys, `return \`${templateString}\`;`);
+  return templateFunction(...values);
+};
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,19 @@
-const makeTemplate = require('../index.js'),
-	assert = require('assert')
+import { saveTemplate, evalTemplate } from "../index.js";
+import { strictEqual } from "assert";
 
-suite(`ES6 template literals`, function(){
-	test(`work`, function(){
-		const greeting = makeTemplate('Hi ${firstName}', {firstName: 'Joe'});
-		assert.equal(greeting, 'Hi Joe')
-	})
-})
+suite(`ES6 template literals`, function () {
+  test(`work`, function () {
+    const greeting1 = saveTemplate("Hi ${firstName}", { firstName: "Joe" });
+    strictEqual(greeting1, "Hi Joe");
+
+    const greeting2 = saveTemplate("Hi ${firstName.substring(1)}", {
+      "firstName.substring(1)": "Joe",
+    });
+    strictEqual(greeting2, "Hi Joe");
+
+    const greeting3 = evalTemplate("Hi ${firstName.substring(1)}", {
+      firstName: "Joe",
+    });
+    strictEqual(greeting3, "Hi oe");
+  });
+});
